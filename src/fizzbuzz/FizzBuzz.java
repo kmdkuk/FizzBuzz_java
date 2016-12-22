3deb1ce746738b4c5e0b77196bd457ee01f685ec
kouki <kouworld0123@gmail.com>
2016-12-22 13:55:39 +0900

    関数fizzbuzzを追加 引数iがどれに対応しているか

--- src/fizzbuzz/FizzBuzz.java
+++ src/fizzbuzz/FizzBuzz.java
@@ -6,8 +6,21 @@ public class FizzBuzz {
 		// TODO 自動生成されたメソッド・スタブ
 		for(int i=1;i<100;i++)
 		{
-			System.out.println(i);
+			System.out.println(fizzbuzz(i));
 		}
 	}
-
+	public static String fizzbuzz(int i)
+	{
+		if(i%3 == 0 && i%5 == 0)
+		{
+			return "fizzbuzz";
+		}else if(i%3 == 0)
+		{
+			return "fizz";
+		}else if(i%5 == 0)
+		{
+			return "buzz";
+		}
+		return Integer.toString(i);
+	}
 }
